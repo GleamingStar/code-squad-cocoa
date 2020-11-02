ba1cb1c1526a711fe2670007c910e23f74589496
unknown <tjdduddl10@gmail.com>
2020-11-02 16:22:21 +0900

    lucas - cocoa 1일차 미션 3번까지

--- 1102/GetAreaApp.js
+++ 1102/GetAreaApp.js
@@ -1,29 +1,42 @@
 const { get } = require("http");
 
+var executionSequence = [];
+
+function printExecutionSecuence() {
+    for (var i=0; i<executionSequence.length; i++)
+        console.log(executionSequence[i]);
+}
 function getArea(sort, arg1, arg2, arg3) {
-    var sequenceArray = [];
-
+    var result = 0;
     if(sort === 'circle') {
-        return this.getCircle(arg1, arg2);
+        result = this.getCircle(arg1, arg2);
+    } else if (sort === 'rect') {
+        result = this.getRectangle(arg1,arg2);
+    } else if (sort === 'trapezoid') {
+        result = this.getTrapezoid(arg1, arg2, arg3);
+    } else {
+        result = 'error';
     }
-    if(sort === 'rect') {
-        return arg1*arg2;
-    }
-    if(sort === 'trapezoid') {
-        return (arg1+arg2)*arg3/2;
-    }
-
-} 
+    executionSequence.push(sort+' : '+result);
+    console.log(result);
+};
 getArea.prototype.getCircle = function(radius, radius2) {
     var sum = radius*radius*Math.PI;
     for(var i=radius+1; i<radius2; i++) {
         sum += i*i*Math.PI;
     }
     return sum;
-}
+};
+getArea.prototype.getRectangle = function(base, height) {
+    return base*height;
+};
+getArea.prototype.getTrapezoid = function(base0, base1, height) {
+    return base0*base1*height/2;
+};
 
-getArea.prototype.printExecutionSequence = function() {
-
-}
-console.log(getArea('circle', 2, 4));
-console.log(getArea('rect', 2, 4));
+var test0 = new getArea('circle', 2);
+var test1 = new getArea('circle', 2, 5);
+var test2 = new getArea('rect', 2, 10);
+var test3 = new getArea('trapezoid', 2, 4, 10);
+var test4 = new getArea('a', 'b')
+printExecutionSecuence();
