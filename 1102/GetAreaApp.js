dd42405ec8b071bdac3a1b0c13e2732dca9ce7fe
unknown <tjdduddl10@gmail.com>
2020-11-02 17:31:20 +0900

    getCircle() 수정

--- 1102/GetAreaApp.js
+++ 1102/GetAreaApp.js
@@ -25,7 +25,7 @@ function getArea(sort, arg1, arg2, arg3) {
 };
 getArea.prototype.getCircle = function(radius, radius2) {
     var sum = radius*radius*Math.PI;
-    for(var i=radius+1; i<radius2; i++) {
+    for(var i=radius+1; i<=radius2; i++) {
         sum += i*i*Math.PI;
     }
     return sum;
@@ -38,7 +38,7 @@ getArea.prototype.getTrapezoid = function(base0, base1, height) {
 };
 
 var test0 = new getArea('circle', 2);
-var test1 = new getArea('circle', 2, 5);
+var test1 = new getArea('circle', 1, 2);
 var test2 = new getArea('rect', 2, 10);
 var test3 = new getArea('trapezoid', 2, 4, 10);
 var test4 = new getArea('a', 'b')
