a982d0aef6e732eb4c21dabe4de9779b5487cc86
unknown <tjdduddl10@gmail.com>
2020-11-04 17:47:20 +0900

    체크아웃 과제제출용 현재진척 커밋

--- Day3/ArrayApp.js
+++ Day3/ArrayApp.js
@@ -1,3 +1,4 @@
+//팩토리얼 구현 함수
 function factorial(n) {
     if (n>1) {
         return n*factorial(n-1);
@@ -6,12 +7,75 @@ function factorial(n) {
         return 1;
     }
 }
+//과제 실행 함수
 function calculate(n) {
     let arr = [];
-    for (let i=1; i<=n; n++) {
-        arr.push(factorial[i]);
+    for (let i=1; i<=n; i++) {
+        arr.push(factorial(i));
     }
     console.log(arr);
 }
 console.log(factorial(4));
 calculate(4);
+
+////////////////////////////////////////////
+
+function filterId(arr) {
+    const reg = RegExp(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi);
+    
+    // let filteredArray = arr.filter(function(id) {
+    //     return !reg.test(id);
+    // });
+
+    // let filteredArray2 = [];
+
+    // filteredArray.forEach(function(id) {
+    //     filteredArray2.push(id.replace(/[0-9]/g, ""));
+    // });
+
+    // console.log(filteredArray2);
+
+    let filteredArray = [];
+
+    arr.filter(function(id) {
+        return !reg.test(id);
+    }).forEach(function(id) {
+        filteredArray.push(id.replace(/[0-9]/g, ""));
+    });
+
+    console.log(filteredArray);
+}
+const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"]
+
+filterId(peoples);
+
+
+/////////////////////////////////////////////
+const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
+
+function getAverage(arr) {
+    let newArr = [];
+    for(let i=0; i<arr.length; i++) {
+        let sum = 0;
+        for(let j=0; j<arr[i].length; j++)
+            sum += arr[i][j];
+        newArr.push(Math.floor(sum/arr[i].length));
+    }
+    console.log("학생들의 평균점수는 "+newArr.join('점, ')+"점입니다.");
+
+    let maxArr = arr.map(function(element) {
+        return Math.max.apply(null, element);
+    });
+
+    let maxsum = 0;
+    for(let i = 0; i<maxArr.length; i++)
+        maxsum += maxArr[i];
+    console.log("모든 학생의 최고점수의 평균점수는 "+maxsum/maxArr.length+"점입니다.")
+}
+
+getAverage(grades);
+
+///////////////////////////////////////
+
+
+
--- Day3/JinsooApp.js
+++ Day3/JinsooApp.js
@@ -4,9 +4,9 @@ class Jinsoo{
     };
 
     solution(base, number, person) {
-        this.print(this.convert(base,number,person));
+        this.print(this.convert(base, number, person));
     };
-
+    
     getMyTurn(base, number, person, turn){
         console.log('길동이 말해야 할 수는 ')
         this.print(this.filter(base, number, person, turn));
@@ -21,20 +21,21 @@ class Jinsoo{
         }
         return answerArray;
     };
-
-    print(array) {
-        console.log('\"'+array.join('\", \"')+'\"');
-    };
-
+    
     filter(base, number, person, turn) {
         let newArr = [];
         for(let i = 0; i<number*person; i++)
             if (i%person === turn-1)
                 newArr.push(this.convert(base, number, person)[i]);
         return newArr;
+    };
+    
+    print(array) {
+        console.log('\"'+array.join('\", \"')+'\"');
     }
+
 };
 
 const test = new Jinsoo();
-test.solution(16,10,3);
-test.getMyTurn(2,5,3,2);
+test.solution(16, 10, 4);
+test.getMyTurn(2, 5, 3, 1);
