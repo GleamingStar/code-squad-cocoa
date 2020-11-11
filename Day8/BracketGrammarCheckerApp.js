374c074984111d416e1ff1cea4fe3d1f2215579b
unknown <tjdduddl10@gmail.com>
2020-11-11 15:05:28 +0900

    feat : add printStructure() method
    ,를 replace하지말고 이용할 것, 배열이 닫히고 다시 원소가 추가되거나 배열이 다시 열리는 케이스 고려할 것

--- Day8/BracketGrammarCheckerApp.js
+++ Day8/BracketGrammarCheckerApp.js
@@ -1,20 +1,20 @@
-
-//////////어떻게 하면 stack 자료구조를 활용할 수 있을까
-//////////3. (선택) 배열 분석 정보를 출력한다. 구현필요
-
 class BracketGrammarChecker {
     stack = [];
     elementCount = 0;
+    constructor(data) {
+        this.data = data
+    }
 
-    run(data) {
-        if (!this.checkBracket(data))
+    run() {
+        if (!this.checkBracket())
             return console.log('닫는 괄호가 일치하지 않습니다.');
-        this.pushData(data);
+        this.pushData();
         this.printResult();
+        this.printStructure();
     };
 
-    pushData(data) {
-        const newData = data.replace(/,/g, '');
+    pushData() {
+        const newData = this.data.replace(/,/g, '');
         let counts = 0;
         for (let i in newData) {
             counts++;
@@ -27,10 +27,10 @@ class BracketGrammarChecker {
         }
     };
 
-    checkBracket(data) {
+    checkBracket() {
         let openBracket = 0;
         let closedBracket = 0;
-        for (let i of data) {
+        for (let i of this.data) {
             if (i === "[")
                 openBracket++;
             if (i === "]")
@@ -39,17 +39,27 @@ class BracketGrammarChecker {
         return openBracket === closedBracket;
     };
 
+    printStructure() {
+        console.log(`{\n` + `  "type" : "root",\n` + `  "child : [`);
+        for (let i = 0; i < this.stack.length; i++) {
+            console.log(`{\n` + `  "type" : "array",\n` + `  "child" : [ `)
+            for (let j = 0; j < this.stack[i].length; j++)
+                console.log(`{\n` + `  "type": "${typeof this.stack[i][j]}",\n` + `  "value" : "${this.stack[i][j]}",\n` + `  "child" : []\n` + `},`);
+        }
+        for (let i = 0; i < this.stack.length + 1; i++)
+            console.log(`]\n` + `}`);
+    };
+
     printResult() {
+        console.log(this.stack)
         return console.log(`깊이 수준은 ${this.stack.length}이며, 총 ${this.elementCount}개의 원소가 포함되어 있습니다.`);
-    }
-
+    };
 };
-
-const test = new BracketGrammarChecker();
-
 const data = "[1,2,[3,4,[5,[6]]]]";
 const data1 = "[1,2,[3,4,[5,[6]]";
-const data2 = "[1[2[3[45678[9[10]]]]]]";
-test.run(data);
-test.run(data1);
-test.run(data2);
+const data2 = "[1,[2,[3,[4,5,6,[7,[8]]]]]]";
+
+const test = new BracketGrammarChecker(data);
+test.run()
+const test2 = new BracketGrammarChecker(data2)
+test2.run()
