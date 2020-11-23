2983c7c364f31386e0d13f7a4a243a5de7717fcf
unknown <tjdduddl10@gmail.com>
2020-11-23 15:25:40 +0900

    refactor : add SmartDropDown class
    prototype 으로 리팩토링
    
    중복제거, 전역변수 제거 및 CSS 수정 요망

--- Day16/smartdropdown.js
+++ Day16/smartdropdown.js
@@ -1,46 +1,52 @@
 const ul = document.querySelector("ul");
-const list = document.querySelector('#list')
+const list = document.querySelector('#list');
 const topList = document.querySelector("#toplist");
 const consoleBox = document.querySelector('#consoleBox');
 
-const box = {};
-let onOff = 'off';
-let isDelayed = true;
-
-setInterval(() => isDelayed = true, 500)
-
-refreshList = () => {
-    const template = []
-    for(key in box) {
-        template.push(key + ' : ' + box[key])
-    }
-    consoleBox.innerHTML = template.join('<br>');
+const SmartDropDown = function () {
+    this.isOn = false;
+    this.isDelayed = true;
+    this.box = {};
+    this.init();
 }
 
-const onEvent = () => {
-    if (onOff === "on")
+SmartDropDown.prototype.init = function () {
+    setInterval(() => this.isDelayed = true, 500)
+
+    topList.addEventListener("mouseenter", () => this.isOn = true);
+
+    ul.addEventListener("mouseleave", () => {
+        this.onOff = false;
+        list.style.display = "none";
+    });
+
+    topList.addEventListener("mouseover", () => setTimeout(this.onEvent.bind(this), 1000));
+
+    list.addEventListener("mousemove", this.overEvent.bind(this));
+}
+
+SmartDropDown.prototype.refreshList = function () {
+    const template = [];
+    for (key in this.box)
+        template.push(key + ' : ' + this.box[key]);
+    consoleBox.innerHTML = template.join('<br>');
+}
+SmartDropDown.prototype.onEvent = function () {
+    if (this.isOn)
         list.style.display = "block";
 }
 
-const overEvent = (e) => {
+SmartDropDown.prototype.overEvent = function (e) {
     const text = e.target.innerHTML;
-    if (isDelayed && text.length < 20) {
-        if (box[text])
-            box[text]++;
+    if (this.isDelayed && text.length < 20) {
+        if (this.box[text])
+            this.box[text]++;
         else
-            box[text] = 1
-        refreshList()
-        isDelayed = false;
+            this.box[text] = 1;
+        this.refreshList();
+        this.isDelayed = false;
     }
-};
+}
 
-topList.addEventListener("mouseenter", () => onOff = "on");
+new SmartDropDown();
 
-ul.addEventListener("mouseleave", () => {
-    onOff = 'off';
-    list.style.display = "none";
-});
-
-topList.addEventListener("mouseover", () => setTimeout(onEvent, 1000));
-
-list.addEventListener("mousemove", overEvent)
